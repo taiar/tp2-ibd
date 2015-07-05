48cf4a6ebd1ac7422a98044037e65fb6f14f21b0
André Taiar <taiar@hotmail.com>
2015-07-05 20:22:42 -0300

    Adicionando página com descrição do trabalho.

--- web/application/views/paginas/dados.php
+++ web/application/views/paginas/dados.php
@@ -2,6 +2,46 @@
 
 <h2 class="sub-header">Dados</h2>
 
+<p>
+  Os dados utilizados para este trabalho foram obtidos em
+  <a href="http://dados.gov.br/dataset/reclamacoes-do-consumidor-gov-br">
+    http://dados.gov.br/dataset/reclamacoes-do-consumidor-gov-br</a> e contém
+  registros nacionais de reclamações de usuários de Agosto de 2014 a Janeiro de
+  2015 sobre os serviços prestados por diversas empresas. A base é mantida pela
+  SENACON - Secretaria Nacional do Consumidor.
+</p>
+
+<p>
+  Os dados foram corretamente normalizados e separados nas tabelas gerando mais
+  de 94700 registros. Existem dados de mais de 200 empresas e mais de 45000
+  reclamações de consumidores foram analizadas.
+</p>
+
 <h2 class="sub-header">Instruções</h2>
+<p>
+  No menu lateral encontram-se as queries que foram escritas para o trabalho.
+  Cada link representa uma query. Ao clicar nelas, uma tela se abrirá contendo
+  o SQL, os resultados e algum eventual conteúdo dinâmico de cada uma das
+  consultas.
+</p>
+
+<p>
+  No menu superior encontram-se informações adicionais sobre o trabalho bem como
+  um ambiente parecido com o <em>Praticando SQL</em> que utilizamos durante o
+  semestre, porém com a base de dados que escolhemos para o nosso trabalho.
+</p>
 
 <h2 class="sub-header">Recursos</h2>
+<p>Recusos utilizados para desenvolvimento do trabalho:</p>
+<ul>
+  <li><a href="https://atom.io/" target="_blank">Atom Editor</a></li>
+  <li><a href="https://git-scm.com/" target="_blank">Git</a></li>
+  <li><a href="https://github.com/" target="_blank">Github</a></li>
+  <li><a href="https://jquery.com/" target="_blank">Jquery</a></li>
+  <li><a href="http://getbootstrap.com/" target="_blank">Bootstrap</a></li>
+  <li><a href="http://www.codeigniter.com/" target="_blank">Codeigniter</a></li>
+  <li><a href="http://www.php.net/" target="_blank">PHP</a></li>
+  <li><a href="http://www.mysql.com/" target="_blank">Mysql</a></li>
+  <li><a href="http://www.libreoffice.org/" target="_blank">Libre Office</a></li>
+  <li><a href="http://linuxmint.com/" target="_blank">Linux Mint</a></li>
+</ul>
